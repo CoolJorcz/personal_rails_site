ebac48fb87a16c4a0281a4f91705675a2b58b700
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-12-23 10:10:44 -0600

    Well, that was easier done than said. Debugged problem due to semantic versioning going from 4.0 to 5.0

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
 	belongs_to :category
 	scope :rails, -> { where(category_id: 1)}
 	extend FriendlyId
-	friendly_id :title, use: :slugged
+	friendly_id :title, use: [:slugged, :finders]
 end
